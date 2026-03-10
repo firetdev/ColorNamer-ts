948a1ebffa28bf03e94cf362a2dfedc4c565d238
firetdev <futuregamesstore@gmail.com>
2026-03-10 17:03:18 -0500

    Modified syntax

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,7 +10,7 @@ const hexToRgb = (hex: string) => {
   } : { r: 0, g: 0, b: 0 };
 };
 
-function App() {
+const App: React.FC = () => {
   const INITIAL_COLOR = '#3498db';
 
   const [color, setColor] = useState<string>(INITIAL_COLOR);
